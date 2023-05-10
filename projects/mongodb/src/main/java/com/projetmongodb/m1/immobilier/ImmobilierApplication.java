297881b9ce651f8a3ea89af044282365227e4cef
Fayssal EL ANSARI <fayssal.el.ansari@gmail.com>
2023-05-10 15:13:20 +0200

    start

---
+++ projects/mongodb/src/main/java/com/projetmongodb/m1/immobilier/ImmobilierApplication.java
@@ -0,0 +1,13 @@
+package com.projetmongodb.m1.immobilier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobilierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmobilierApplication.class, args);
+	}
+
+}
---
+++ projects/mongodb/src/test/java/com/projetmongodb/m1/immobilier/ImmobilierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetmongodb.m1.immobilier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobilierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
