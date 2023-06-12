8dff6aaeb00f82b8d9ede8644471b8cd0895d4e5
Fayssal EL ANSARI <fayssal.el.ansari@gmail.com>
2023-06-12 17:47:30 +0200

    SideBar done :eyes:

--- projects/dashboard/mern_dashboard/client/src/components/Navbar.jsx
+++ projects/dashboard/mern_dashboard/client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ import {
   useTheme,
 } from "@mui/material";
 
-const Navbar = () => {
+const Navbar = (isSidebarOpen, setIsSidebarOpen) => {
   const dispatch = useDispatch();
   const theme = useTheme();
   return (
@@ -33,7 +33,7 @@ const Navbar = () => {
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT SIDE */}
         <FlexBetween>
-          <IconButton onClick={() => console.log("open/close sidebar")}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
@@ -56,6 +56,9 @@ const Navbar = () => {
             ) : (
               <LightModeOutlined sx={{ fontSize: "25px" }} />
             )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
           </IconButton>
         </FlexBetween>
       </Toolbar>
