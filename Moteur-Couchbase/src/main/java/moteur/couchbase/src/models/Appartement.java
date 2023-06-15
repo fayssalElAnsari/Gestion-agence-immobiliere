2da0f7c2232928bdba6c09f70bf759a1a8c56e38
JojoGit01 <jonathan-08@orange.fr>
2023-06-15 03:20:36 +0200

    Models Couchbase props, get, set - Ok

---
+++ Moteur-Couchbase/src/main/java/moteur/couchbase/src/models/Appartement.java
@@ -0,0 +1,78 @@
+package moteur.couchbase.src.models;
+
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.kv.GetResult;
+import com.couchbase.client.java.query.QueryOptions;
+import com.couchbase.client.java.query.QueryResult;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Appartement {
+
+    private String id;
+    private String userId;
+    private double price;
+    private String location;
+    private int numberOfRooms;
+    private int numberOfBathrooms;
+    private List<Reservation> reservations;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public int getNumberOfBathrooms() {
+        return numberOfBathrooms;
+    }
+
+    public void setNumberOfBathrooms(int numberOfBathrooms) {
+        this.numberOfBathrooms = numberOfBathrooms;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+}
