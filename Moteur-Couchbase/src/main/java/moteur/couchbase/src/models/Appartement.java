b40a01cf562f62c7615f7fbd0a8f83f579cbb9b9
JojoGit01 <jonathan-08@orange.fr>
2023-06-15 04:57:21 +0200

    ..

--- Moteur-Couchbase/src/main/java/moteur/couchbase/src/models/Appartement.java
+++ Moteur-Couchbase/src/main/java/moteur/couchbase/src/models/Appartement.java
@@ -1,23 +1,79 @@
 package moteur.couchbase.src.models;
 
-import com.couchbase.client.java.Cluster;
-import com.couchbase.client.java.Collection;
-import com.couchbase.client.java.kv.GetResult;
-import com.couchbase.client.java.query.QueryOptions;
-import com.couchbase.client.java.query.QueryResult;
-
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Appartement {
 
     private String id;
-    private String userId;
-    private double price;
-    private String location;
+    private String title;
+    private String description;
+    private double defaultPrice;
     private int numberOfRooms;
     private int numberOfBathrooms;
-    private List<Reservation> reservations;
+    private double area;
+    private String address;
+    private Location location;
+    private String ownerId;
+    private List<String> images;
+    private List<Rate> rates;
+    private String createdAt;
+    private String updatedAt;
+
+    // getters and setters
+
+    public static class Location {
+        private double latitude;
+        private double longitude;
+
+        public double getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(double latitude) {
+            this.latitude = latitude;
+        }
+
+        public double getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(double longitude) {
+            this.longitude = longitude;
+        }
+// getters and setters
+    }
+
+    public static class Rate {
+        private String startDate;
+        private String endDate;
+        private double price;
+
+        public String getStartDate() {
+            return startDate;
+        }
+
+        public void setStartDate(String startDate) {
+            this.startDate = startDate;
+        }
+
+        public String getEndDate() {
+            return endDate;
+        }
+
+        public void setEndDate(String endDate) {
+            this.endDate = endDate;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+// getters and setters
+    }
 
     public String getId() {
         return id;
@@ -27,28 +83,28 @@ public class Appartement {
         this.id = id;
     }
 
-    public String getUserId() {
-        return userId;
+    public String getTitle() {
+        return title;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public double getPrice() {
-        return price;
+    public String getDescription() {
+        return description;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public String getLocation() {
-        return location;
+    public double getDefaultPrice() {
+        return defaultPrice;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public void setDefaultPrice(double defaultPrice) {
+        this.defaultPrice = defaultPrice;
     }
 
     public int getNumberOfRooms() {
@@ -67,12 +123,67 @@ public class Appartement {
         this.numberOfBathrooms = numberOfBathrooms;
     }
 
-    public List<Reservation> getReservations() {
-        return reservations;
+    public double getArea() {
+        return area;
     }
 
-    public void setReservations(List<Reservation> reservations) {
-        this.reservations = reservations;
+    public void setArea(double area) {
+        this.area = area;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public List<Rate> getRates() {
+        return rates;
+    }
+
+    public void setRates(List<Rate> rates) {
+        this.rates = rates;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
--- Moteur-Couchbase/src/main/java/moteur/couchbase/src/services/AppartementService.java
+++ Moteur-Couchbase/src/main/java/moteur/couchbase/src/services/AppartementService.java
@@ -8,38 +8,38 @@ import com.couchbase.client.java.kv.GetResult;
 import com.couchbase.client.java.kv.MutationResult;
 import com.couchbase.client.java.query.QueryOptions;
 import com.couchbase.client.java.query.QueryResult;
-import moteur.couchbase.src.models.Appartement;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AppartementService {
     private final Cluster cluster;
     private final Collection appartementCollection;
 
-    public AppartementService(Cluster cluster, String database, String collection) {
+    public AppartementService(Cluster cluster, String database, Collection collection) {
         this.cluster = cluster;
-        this.appartementCollection = cluster.bucket(database).defaultCollection();
+        this.appartementCollection = collection;
     }
 
     // Methodes CRUD
 
     // Creation
 
-    public void createAppartement(Appartement appartement) {
-        appartementCollection.insert(appartement.getId(), appartement);
+    public void createAppartement(JsonObject appartement) {
+        appartementCollection.insert(appartement.getString("_id"), appartement);
     }
 
-    public void createAppartements(List<Appartement> appartements) {
+    public void createAppartements(List<JsonObject> appartements) {
         appartements.forEach(this::createAppartement);
     }
 
     // Lecture
 
-    public Appartement getAppartement(String id) {
+    public JsonObject getAppartement(String id) {
         try {
             GetResult result = appartementCollection.get(id);
-            return result.contentAs(Appartement.class);
+            return result.contentAsObject();
         } catch (DocumentNotFoundException e) {
             return null;
         }
@@ -47,11 +47,11 @@ public class AppartementService {
 
     // Mise à jour
 
-    public void updateAppartement(Appartement appartement) {
-        appartementCollection.replace(appartement.getId(), appartement);
+    public void updateAppartement(JsonObject appartement) {
+        appartementCollection.replace(appartement.getString("_id"), appartement);
     }
 
-    public void updateAppartements(List<Appartement> appartements) {
+    public void updateAppartements(List<JsonObject> appartements) {
         appartements.forEach(this::updateAppartement);
     }
 
@@ -67,34 +67,34 @@ public class AppartementService {
 
     // Methodes de recherche
 
-    public List<Appartement> findAppartementsByUserId(String userId) {
-        String statement = String.format("SELECT * FROM `%s` WHERE userId = $userId", appartementCollection.name());
+    public List<JsonObject> findAppartementsByUserId(String userId) {
+        String statement = String.format("SELECT * FROM `%s` WHERE ownerId = $userId", appartementCollection.name());
         QueryResult result = cluster.query(
                 statement,
-                QueryOptions.queryOptions().parameters((JsonObject) Map.of("userId", userId))
+                QueryOptions.queryOptions().parameters(JsonObject.create().put("userId", userId))
         );
 
-        return result.rowsAs(Appartement.class);
+        return result.rowsAsObject().stream().collect(Collectors.toList());
     }
 
-    public List<Appartement> findAppartementsByNumberOfRooms(int numberOfRooms) {
+    public List<JsonObject> findAppartementsByNumberOfRooms(int numberOfRooms) {
         String statement = String.format("SELECT * FROM `%s` WHERE numberOfRooms = $numberOfRooms", appartementCollection.name());
         QueryResult result = cluster.query(
                 statement,
-                QueryOptions.queryOptions().parameters((JsonObject) Map.of("numberOfRooms", numberOfRooms))
+                QueryOptions.queryOptions().parameters(JsonObject.create().put("numberOfRooms", numberOfRooms))
         );
 
-        return result.rowsAs(Appartement.class);
+        return result.rowsAsObject().stream().collect(Collectors.toList());
     }
 
-    public List<Appartement> findAppartementsByPriceRange(double minPrice, double maxPrice) {
-        String statement = String.format("SELECT * FROM `%s` WHERE price >= $minPrice AND price <= $maxPrice", appartementCollection.name());
+    public List<JsonObject> findAppartementsByPriceRange(double minPrice, double maxPrice) {
+        String statement = String.format("SELECT * FROM `%s` WHERE defaultPrice >= $minPrice AND defaultPrice <= $maxPrice", appartementCollection.name());
         QueryResult result = cluster.query(
                 statement,
-                QueryOptions.queryOptions().parameters((JsonObject) Map.of("minPrice", minPrice, "maxPrice", maxPrice))
+                QueryOptions.queryOptions().parameters(JsonObject.create().put("minPrice", minPrice).put("maxPrice", maxPrice))
         );
 
-        return result.rowsAs(Appartement.class);
+        return result.rowsAsObject().stream().collect(Collectors.toList());
     }
 
 }
